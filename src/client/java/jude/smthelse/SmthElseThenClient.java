0ea7a27dd6c7d8f3ac3c68250648ff658b7f04e7
KaylaDoesStuff <kayla.trevena982@gmail.com>
2024-07-01 05:07:53 +0000

    initial commit

---
+++ src/client/java/jude/smthelse/SmthElseThenClient.java
@@ -0,0 +1,10 @@
+package jude.smthelse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SmthElseThenClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
